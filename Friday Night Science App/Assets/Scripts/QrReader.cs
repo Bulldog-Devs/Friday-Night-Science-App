d73eb89aad3de61bacf46cf51b81ea64723619bd
Jaron Burtnett <43459765+HylianAdventurer@users.noreply.github.com>
2018-11-01 16:35:49 -0700

    Qr code reader (#1)
    * Added Camera
    
    Added a Camera Function that shows what the camera can see on the screen.
    
    * Base code foundation
    
    Added QRZing for qrcode
    Added QRReader and GenerateQR
    
    * Working QR Reader with Webcam
    
    This build will open the devices camera and read a QR code from it and ouput its result in the Debug
    
    * QR Reader V1.0
    
    Fully functional QR Reader with:
    -Scene Changing
    -Globally Saved QR Input
    -Manual Input

---
+++ Friday Night Science App/Assets/Scripts/QrReader.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using ZXing;
+using ZXing.QrCode;
+
+public class QRReader : MonoBehaviour
+{
+    private bool camAvailable;
+    private WebCamTexture backCam;
+    private int cooldown = 0;
+    public RawImage background;
+    public AspectRatioFitter fit;
+    public QRUIControl control;
+
+    private void Start()
+    {
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        if (devices.Length == 0)
+        {
+            Debug.Log("No camera detected");
+            camAvailable = false;
+            return;
+        }
+
+        for (int i = 0; i < devices.Length; i++)
+        {
+            backCam = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
+            if (!devices[i].isFrontFacing)
+            {
+                backCam = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
+            }
+        }
+
+        if (backCam == null)
+        {
+            Debug.Log("Unable to find back camera");
+            return;
+        }
+
+        backCam.Play();
+        background.texture = backCam;
+        camAvailable = true;
+    }
+
+    private void Update()
+    {
+        if (!camAvailable){return;}
+
+        fit.aspectRatio = (float)backCam.width / (float)backCam.height;
+
+        float scaleY = backCam.videoVerticallyMirrored ? -1f : 1f;
+        background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
+
+        int orient = -backCam.videoRotationAngle;
+
+        background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
+
+        if(cooldown==0)
+        {
+            try
+            {
+                cooldown = 35;
+                IBarcodeReader barcodeReader = new BarcodeReader();
+
+                var result = barcodeReader.Decode(backCam.GetPixels32(), backCam.width, backCam.height);
+                if (result != null)
+                {
+                    PlayerPrefs.SetString("QRInput", result.Text);
+                    Debug.Log("Text from QR code: " + PlayerPrefs.GetString("QRInput"));
+                    control.LoadScene("Quiz Placeholder");
+                }
+            } catch (System.Exception e)
+            {
+                Debug.LogWarning(e.Message);
+            }
+        }
+        cooldown--;
+    }
+}
---
+++ Friday Night Science App/Assets/Scripts/SetText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetText : MonoBehaviour {
+
+    public Text screenText;
+    
+    // Use this for initialization
+	void Start () {
+        if(!PlayerPrefs.GetString("QRInput").Equals(""))
+        {
+            screenText.text = PlayerPrefs.GetString("QRInput");
+        } else
+        {
+            screenText.text = "QRInput is empty";
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
