f0b75cfdad17e3a9debfd1c42e282585b3d7fe0a
HylianAdventurer <jaronburtnett@gmail.com>
2018-11-07 22:28:35 -0800

    Pre-Alpha
    A Fully functional App, with a map, QRReader and Quiz

---
+++ Friday Night Science App/Assets/Scripts/Game.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour {
+
+    public string QRScene = "QRReader";
+    public string QuizScene = "QuizMain";
+    public string MapScene = "MapMain";
+    public string QRInput = "QRInput";
+
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene("Scenes/" + name, LoadSceneMode.Single);
+    }
+
+    public void enterInput(string store, string input)
+    {
+        PlayerPrefs.SetString(store, input);
+    }
+
+    public void enterInput(string input)
+    {
+        enterInput(QRInput, input);
+        LoadScene(QuizScene);
+    }
+}
---
+++ Friday Night Science App/Assets/Scripts/HighlightControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightControl : MonoBehaviour {
+
+    
+
+    // Use this for initialization
+    void Start () {
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(NextButton.resetHighlight == "y")
+        {
+            GetComponent<ParticleSystem>().Stop();
+
+        }
+        if (NextButton.resetHighlight == "n")
+        {
+            GetComponent<ParticleSystem>().Play();
+        }
+		
+	}
+
+}
+    
