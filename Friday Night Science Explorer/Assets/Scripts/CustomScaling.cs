db751e253d82336411787382fe8a957be35679be
HylianAdventurer <jaronburtnett@gmail.com>
2019-04-19 17:30:51 -0700

    Full App v2.0
    Added some files to help with scaling.

---
+++ Friday Night Science Explorer/Assets/Scripts/CustomScaling.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomScaling : MonoBehaviour {
+
+    public GameObject scaleBase;
+
+    public bool doWidthPercent = true;
+    public int widthPercent;
+    public bool doHeightPercent = true;
+    public int heightPercent;
+
+    // Use this for initialization
+    void Start() {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (doWidthPercent && doHeightPercent)
+        {
+            gameObject.GetComponent<RectTransform>().sizeDelta.Set(scaleBase.GetComponent<RectTransform>().sizeDelta.x * widthPercent / 100, scaleBase.GetComponent<RectTransform>().sizeDelta.y * heightPercent / 100);
+        }
+        else if (doWidthPercent)
+        {
+            gameObject.GetComponent<RectTransform>().sizeDelta.Set(scaleBase.GetComponent<RectTransform>().sizeDelta.x * widthPercent / 100, gameObject.GetComponent<RectTransform>().sizeDelta.y);
+        }
+        else if (doHeightPercent)
+        {
+            gameObject.GetComponent<RectTransform>().sizeDelta.Set(gameObject.GetComponent<RectTransform>().sizeDelta.x, scaleBase.GetComponent<RectTransform>().sizeDelta.y * heightPercent / 100);
+        }
+    }
+}
---
+++ Friday Night Science Explorer/Assets/Scripts/HeightWidthRatio.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeightWidthRatio : MonoBehaviour {
+
+    public bool heightAffectsWidth = true;
+    public int heightRatio, widthRatio;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(heightAffectsWidth)
+        {
+            gameObject.GetComponent<RectTransform>().sizeDelta.Set(gameObject.GetComponent<RectTransform>().sizeDelta.y * widthRatio / heightRatio, gameObject.GetComponent<RectTransform>().sizeDelta.y);
+        }
+        else
+        {
+            gameObject.GetComponent<RectTransform>().sizeDelta.Set(gameObject.GetComponent<RectTransform>().sizeDelta.x, gameObject.GetComponent<RectTransform>().sizeDelta.x * heightRatio / widthRatio);
+        }
+	}
+}
